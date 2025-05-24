2584f7c722129d2967c3740893b405efc44c5544
Alex Pavlov <yukiu217@gmail.com>
2025-05-24 12:36:11 +0300

    DAO RedirectURL

--- api_v1/redirect_servise/dao.py
+++ api_v1/redirect_servise/dao.py
@@ -1,8 +1,9 @@
 from config.dao import BaseDAO
+from config.models import RedirectURL
 
 
 class RedirectServiseDAO(BaseDAO):
     """
     Class DAO for redirect servise
     """
-    model = ...
+    model = RedirectURL
