6a985d9ba2400f80788f3d8fadba6b286646918e
Alex Pavlov <yukiu217@gmail.com>
2025-05-24 16:51:29 +0300

    add endpoint for create url

--- api_v1/redirect_servise/views.py
+++ api_v1/redirect_servise/views.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, status, Depends
 from fastapi_cache.decorator import cache
 from fastapi.responses import RedirectResponse
 
+from loguru import logger
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from config import db_connection
@@ -23,3 +25,17 @@ router = APIRouter(
 @cache(expire=settings.MAX_CACHE_EXPIRE)
 async def get_list_urls(session: AsyncSession = Depends(db_connection.session_geter)):
     return await RedirectServiseDAO.find_all_items_by_args(session=session)
+
+
+@router.post(path='',
+             name='urls:create',
+             response_model=ViewUrlSchema,
+             status_code=status.HTTP_202_ACCEPTED,
+             )
+async def create_short_url(short_url: UrlSchema,
+                           session: AsyncSession = Depends(db_connection.session_geter)):
+    logger.info(f'POST method get data {short_url}')
+    return await RedirectServiseDAO.add(
+        session=session,
+        url=short_url.url,
+    )
