837430ec38ef328797d269d6efdcddbcda62c6a9
Alex Pavlov <yukiu217@gmail.com>
2025-05-24 12:35:27 +0300

    add url model

---
+++ config/models/__init__.py
@@ -0,0 +1,4 @@
+from .urls import RedirectURL
+
+
+__all__ = ('RedirectURL',)
---
+++ config/models/urls.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.types import String
+
+from .base import Base
+
+
+class RedirectURL(Base):
+    """
+    Model RedirectUrl
+    """
+    url: Mapped[str] = mapped_column(String(256))
